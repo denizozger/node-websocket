f17b88ee421920d9f8851435423d24b8c5fcd487
denizozger <deniz@parallelbrains.com>
2013-11-05 10:30:58 +0000

    Server receives some POST data and displays it on clients

--- server.js
+++ server.js
@@ -6,22 +6,71 @@ var WebSocketServer = require('ws').Server
 
 app.use(express.static(__dirname + '/'));
 
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
+app.use(express.bodyParser());
+
 var server = http.createServer(app);
 server.listen(port);
 
-console.log('http server listening on %d', port);
+console.log('HTTP server listening on port %d', port);
 
 var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
+console.log('WebSocket server created');
 
-    console.log('websocket connection open');
+var matchData;
+var clients = [];
 
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
+function removeFromArray(arr, item) {
+    for(var i = arr.length; i--;) {
+        if(arr[i] === item) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+// Receiving new data and pushing it to connected clients
+app.post("/match", function(req, res) {
+   matchData = req.body.newMatchData;
+
+   if (clients !== undefined) { // if at least one client is connected
+      wss.broadcast(JSON.stringify(matchData));
+   }
+
+   res.writeHead(200, {"Content-Type": "text/plain"});
+   res.write("Received match details (" + matchData + ")");
+   res.end(); 
+});
+
+// Client connection
+wss.on('connection', function(client) {
+    console.log('WebSocket connection open'); 
+    
+    clients.push(client);
+
+    console.log('clients size: ' + clients.length);
+
+    for(var i in this.clients) {
+        console.log(this.clients[i]._socket._connecting);
+    }
+    
+    client.send(JSON.stringify('Some initial data'), function() { });
+
+    client.on('close', function() {
+        console.log('WebSocket connection close');
+        removeFromArray(clients, client);
+
+        console.log('clients size: ' + clients.length);
     });
 });
+
+wss.broadcast = function(data) {
+    for(var i in this.clients) {
+        this.clients[i].send(data);
+    }
+};
+
+
